366a7f86b9e9b9a7e81c177bdfd96134220f297a
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-04-27 16:14:14 -0300

    Requisitos 19 a 24 concluídos

--- src/pages/ExploreFoodIngredients.js
+++ src/pages/ExploreFoodIngredients.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreFoodIngredients() {
   return (
     <div>
       <Header title="Explore Ingredients" />
       <p>Explore Food Ingredients</p>
+      <Footer />
     </div>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Foods() {
   return (
     <div>
       <Header hasSearch title="Foods" />
+      <Footer />
     </div>
   );
 }
