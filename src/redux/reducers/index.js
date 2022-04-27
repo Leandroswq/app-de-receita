630cc998e19d14412d0408f0d39307f828a00890
Leandroswq <leandrobonfim15@hotmail.com>
2022-04-27 17:22:29 -0300

    remove user redux
    Co-authored-by: Larissa Rocha de Sant'Anna <LariRdS@users.noreply.github.com>

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import nomeReducer1 from './searchReducer';
-import user from './userReducer';
 
 const rootReducer = combineReducers({
   nomeReducer1,
-  user,
 });
 
 export default rootReducer;
