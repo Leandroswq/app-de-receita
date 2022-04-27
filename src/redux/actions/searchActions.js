33611fedf4a8e376879e80c1098fae3253ebca6c
Wendryo S <wendryo.sales@gmail.com>
2022-04-27 15:20:03 -0300

    REDUX and THUNk configured

---
+++ src/redux/actions/searchActions.js
@@ -0,0 +1,5 @@
+import { USER_LOGIN } from './actionsTypes';
+
+export const api = (value) => ({ type: USER_LOGIN, value });
+
+export const asd = (value) => ({ type: USER_LOGIN, value });
---
+++ src/redux/reducers/searchReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = { teste: 'funcionou' };
+
+const nomeReducer1 = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default nomeReducer1;
