fac1c0a8182aa9292a3b0cece27f748b951431f7
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-04-27 16:33:29 -0300

    Requisitos 67 a 69 concluídos

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,7 +7,20 @@ function Explore() {
   return (
     <div>
       <Header title="Explore" />
-      <p>Explore</p>
+      <Link
+        to="/explore/foods"
+        type="button"
+        data-testid="explore-foods"
+      >
+        Explore Foods
+      </Link>
+      <Link
+        to="/explore/drinks"
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explore Drinks
+      </Link>
       <Footer />
     </div>
   );
