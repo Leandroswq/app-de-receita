fa97f6f4007341ebf7790604d5ea2164e989ea57
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-05-04 14:39:49 -0300

    Requisitos 47 a 49 concluídos

--- src/pages/DrinkRecipeProgress.js
+++ src/pages/DrinkRecipeProgress.js
@@ -1,9 +1,88 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import getDrinks from '../API/getDrinks';
+import shareIcon from '../images/shareIcon.svg';
+import favoritIcon from '../images/whiteHeartIcon.svg';
+import { filterValuesFromObjectToArray } from '../helpers';
 
 function DrinkRecipeProgress() {
+  const { id } = useParams();
+  const [drink, setDrink] = useState(undefined);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await getDrinks(id, 'id');
+      setDrink(response.drinks[0]);
+    }
+    getData();
+  }, []);
+
+  const createIngredientAndMeasureArray = () => {
+    const ingredient = filterValuesFromObjectToArray(/strIngredient/i, drink);
+    const measure = filterValuesFromObjectToArray(/strMeasure/i, drink);
+    console.log(ingredient);
+    console.log(measure);
+    const measureAndIngredient = ingredient.map((item, ind) => {
+      const ing = item[1];
+      const meas = measure[ind] ? measure[ind][1] : '';
+      return [ing, meas];
+    });
+    return measureAndIngredient;
+  };
+
   return (
     <div>
-      <p>Drink Recipe Progress</p>
+      {drink !== undefined && (
+        <>
+          <img
+            src={ drink.strDrinkThumb }
+            alt={ drink.strDrink }
+            data-testid="recipe-photo"
+          />
+          <h2 data-testid="recipe-title">{drink.strDrink}</h2>
+
+          <button type="button" data-testid="share-btn">
+            <img src={ shareIcon } alt="share" />
+          </button>
+          <button type="button" data-testid="favorite-btn">
+            <img src={ favoritIcon } alt="favorite" />
+          </button>
+
+          <p data-testid="recipe-category">{drink.strAlcoholic}</p>
+          <h3>Ingredients</h3>
+          <div>
+            {
+              createIngredientAndMeasureArray()
+                .map((item, ind) => (
+                  <label
+                    key={ `ingredient-${ind}` }
+                    htmlFor={ `ingredient-${ind}` }
+                    data-testid="ingredient-step"
+                  >
+                    <input
+                      type="checkbox"
+                      name={ `ingredient-${ind}` }
+                      id={ `ingredient-${ind}` }
+                    />
+                    {`-${item[0]}-${item[1]}`}
+                  </label>
+                ))
+            }
+          </div>
+          <h3>Instructions</h3>
+          <p data-testid="instructions">{drink.strInstructions}</p>
+
+          <div data-testid={ `${0}-recomendation-card` }>Receitas recomendadas</div>
+          <div data-testid={ `${1}-recomendation-card` }>Receitas recomendadas</div>
+          <button
+            data-testid="finish-recipe-btn"
+            type="button"
+          >
+            Finalizar
+          </button>
+        </>
+      )}
     </div>
   );
 }
