0788ccec841e10826a365c65610909a21d8e9cf0
marqsbruno <marqsbruno@gmail.com>
2022-05-04 18:45:39 -0300

    Requisito 54 feito

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import CardDoneRecipes from '../components/CardDoneRecipes';
 import Header from '../components/Header';
+import { getLocalStorage } from '../helpers';
 
 function DoneRecipes() {
+  const data = getLocalStorage('doneRecipes');
+
   return (
     <div>
       <Header title="Done Recipes" />
-      <p>Done Recipes</p>
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+        >
+          Drink
+        </button>
+      </div>
+      {
+        data.map((elem, i) => (
+          <CardDoneRecipes
+            key={ elem.id }
+            data={ elem }
+            index={ i }
+          />))
+      }
     </div>
   );
 }
